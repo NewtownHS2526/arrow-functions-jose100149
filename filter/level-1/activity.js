af6f3e846e68c89a19e45a2af4eccde5bbf89938
jose100149 <230559303+jose100149@users.noreply.github.com>
2025-12-12 21:32:15 -0500

    Update filter/level-1/activity.js with updated content

--- filter/level-1/activity.js
+++ filter/level-1/activity.js
@@ -110,8 +110,6 @@ Write your solution using:
 
 const exercise1Array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-
-
 // Your solution here (one-line arrow function):
 const exercise1Result1 = exercise1Array.filter(n => n % 2 !== 0);
 // Your solution here (regular arrow function):
@@ -136,11 +134,8 @@ Write your solution using:
 
 const exercise2Array = ["apple", "banana", "kiwi", "orange", "grape"];
 // Your solution here (one-line arrow function):
-// const exercise2Result1 = 
 const exercise2Result1 = exercise2Array.filter(fruit => fruit.length >= 5);
-
 // Your solution here (regular arrow function):
-// const exercise2Result2 = 
 const exercise2Result2 = exercise2Array.filter((fruit) => {
   return fruit.length >= 5;
 });
@@ -163,14 +158,12 @@ Write your solution using:
 const exercise3Array = [15, 8, 22, 5, 30, 12, 18];
 
 // Your solution here (one-line arrow function):
-// const exercise3Result1 = 
-const exercise3Result1 = [15, 12, 18, 22, 30
+const exercise3Result1 = exercise3Array.filter(n => n > 10);
 
 // Your solution here (regular arrow function):
-// const exercise3Result2 = 
-const exercise3Result2 = [5, 8
-
-// Uncomment to test:
+const exercise3Result2 = exercise3Array.filter((n) => {
+  return n > 10;
+});
 console.log("Exercise 3 - One-line:", exercise3Result1);
 console.log("Exercise 3 - Regular:", exercise3Result2);
 Expected: [15, 22, 30, 12, 18]
@@ -191,18 +184,14 @@ Write your solution using:
 const exercise4Array = ["red", "blue", "green", "yellow", "purple"];
 
 // Your solution here (one-line arrow function):
-const exercise4Result1 = [blue
-const exercise4Array = ["red", "blue", "green", "yellow", "purple"];
-
-// Your solution here (one-line arrow function):
-
+const exercise4Result1 = exercise4Array.filter(color => color.startsWith("b"));
 // Your solution here (regular arrow function):
-// const exercise4Result2 = 
-
-// Uncomment to test:
-// console.log("Exercise 4 - One-line:", exercise4Result1);
-// console.log("Exercise 4 - Regular:", exercise4Result2);
-// Expected: ["blue"]
+const exercise4Result2 = exercise4Array.filter((color) => {
+  return color.startsWith("b");
+});
+console.log("Exercise 4 - One-line:", exercise4Result1);
+console.log("Exercise 4 - Regular:", exercise4Result2);
+Expected: ["blue"]
 
 /*
 EXERCISE 5
@@ -218,13 +207,12 @@ Write your solution using:
 const exercise5Array = [2, 4, 6, 8, 10, 11, 12, 13, 14];
 
 // Your solution here (one-line arrow function):
-// const exercise5Result1 = 
+const exercise5Result1 = exercise5Array.filter(n => n % 4 === 0);
 
 // Your solution here (regular arrow function):
-// const exercise5Result2 = 
-
-// Uncomment to test:
-// console.log("Exercise 5 - One-line:", exercise5Result1);
-// console.log("Exercise 5 - Regular:", exercise5Result2);
-// Expected: [4, 8, 12]
-
+const exercise5Result2 = exercise5Array.filter((n) => {
+  return n % 4 === 0;
+});
+console.log("Exercise 5 - One-line:", exercise5Result1);
+console.log("Exercise 5 - Regular:", exercise5Result2);
+Expected: [4, 8, 12]
